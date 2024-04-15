b9d20592b222b7bdfab6490ef2a65330ed290b02
aritellavsn <153779058+aritellavsn@users.noreply.github.com>
2024-04-15 15:55:24 -0700

    hotfix_10

--- packages/ui/src/ui-component/dialog/AboutDialog.jsx
+++ packages/ui/src/ui-component/dialog/AboutDialog.jsx
@@ -52,7 +52,7 @@ const AboutDialog = ({ show, onCancel }) => {
             aria-describedby='alert-dialog-description'
         >
             <DialogTitle sx={{ fontSize: '1rem' }} id='alert-dialog-title'>
-                Flowise Version
+                DevinAI Version
             </DialogTitle>
             <DialogContent>
                 {data && (
@@ -70,10 +70,7 @@ const AboutDialog = ({ show, onCancel }) => {
                                     <TableCell component='th' scope='row'>
                                         {data.currentVersion}
                                     </TableCell>
-                                    <TableCell component='th' scope='row'>
-                                        <a target='_blank' rel='noreferrer' href={data.html_url}>
-                                            {data.name}
-                                        </a>
+                                    <TableCell>.
                                     </TableCell>
                                     <TableCell>{moment(data.published_at).fromNow()}</TableCell>
                                 </TableRow>
